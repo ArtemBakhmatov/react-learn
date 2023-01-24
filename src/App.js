f3cee523f2fc92e05579d4d279026615965c49c9
aretm <ip.bakhmatov@mail.ru>
2023-01-24 20:05:41 +0300

    События в React

--- src/App.js
+++ src/App.js
@@ -1,73 +1,60 @@
-import React from 'react';
+import {Component} from 'react';
 import './App.css';
 
-class App extends React.Component {
+class WhoAmI extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {
-			counter: this.props.counter // при вызове класса <App counter={0} />
-			//counter: 0  // можно прописать значение 0 тут // при вызове класса <App />
-		}
-	}
-	
-	incCounter = () => {  // увеличиваем на единицу
-		if (this.state.counter < 50) {
-			this.setState(state => ({
-				counter: state.counter + 1
-			}));
+		this.state = {  // Это диначеская вещь которая будет менятся (вводим статичные данные)
+			years: 27,
+			text: '+++',
+			position: ''
 		}
 	}
 
-	decCounter = () => {   // уменьшаем на единицу
-		if (this.state.counter > -50) {
-			this.setState(state => ({
-				counter: state.counter - 1
-			}));
-		}
+	nextYear = () => {
+		this.setState(state => ({ // с помощью этого метода будет изменение state -> приходит из this.state
+			years: state.years + 1
+			 
+		}));
 	}
-
-	rndCounter = () => {  // рандомные числа от 50 и до -50
+ 
+	commitInputChanges = (e) => {
+		console.log(e.target.value);
 		this.setState({
-			counter: +(Math.random() * (50 - -50) + -50).toFixed(0)
-		}); // (50 - -50) + -50) -> диапазон от 50 до -50 // toFixed(0) -> округление до целого числа
+			position: e.target.value
+		})
 	}
 
-	resetCounter = () => { // сбросить, тоесть будет ноль
-		this.setState({
-			counter: this.props.counter // при вызове класса <App counter={0} />
-			//counter: 0  // можно прописать значение 0 тут // при вызове класса <App />
-		});
-	}
-	
 	render() {
-		const {counter} = this.state
+		const {name, surname, link} = this.props;
+		const {position, years} = this.state;
+
+		console.log(this);
+		
 		return (
-			<div className="app">
-			<div className="counter">{counter}</div>
-			<div className="controls">
-				<button onClick={this.incCounter}>INC</button>
-				<button onClick={this.decCounter}>DEC</button>
-				<button onClick={this.rndCounter}>RND</button>
-				<button onClick={this.resetCounter}>RESET</button>
-			</div>
+			<div>
+				<button onClick={this.nextYear}>{this.state.text}</button>
+				<h1>My name is {name}, surname - {surname}, 
+				age - {years}, 
+				position - {position}</h1>
+				<a href={link}>My profile</a>
+				<form>
+					<span>Введите должность</span>
+					<input type="text" onChange={this.commitInputChanges}/>
+				</form>
 			</div>
 		)
 	}
 }
+// onChange и onInput в реакте работают одинаково 
+
+function App() {
+	return (
+		<div className='App'>
+			<WhoAmI name='John' surname='Smith' link='facebook.com' />
+			<WhoAmI name='Alex' surname='Shepard' link='vk.com' />
+		</div>
+	);
+}
 
 export default App;
-  
-  // 1) Начальное значение счетчика должно передаваться через props
-  // 2) INC и DEC увеличивают и уменьшают счетчик соответственно на 1. Без ограничений, но можете добавить границу в -50/50. По достижению границы ничего не происходит
-  // 3) RND изменяет счетчик в случайное значение от -50 до 50. Конструкцию можете прогуглить за 20 секунд :) Не зависит от предыдущего состояния
-  // 4) RESET сбрасывает счетчик в 0 или в начальное значение из пропсов. Выберите один из вариантов
-
-// props -> только для чтения
-/* вывод:
-1) у компонента может быть внутреннее состояние, которое динамически меняется
-2) он может быть как у классовых так и функциональных компонентов 
-3) state на прямую менять нельзя, только через команду this.setState
-4) this.setState -> изменение state это асинронная операция
-5) в команде this.setState мы можем менять только те свойства объекта состояния которые нам нужны, остальные 
-остануться без изменения
-*/ 
